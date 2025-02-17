bd7b40ac3e7f189fc678e90078b934db340dfaad
patrickfoulger1 <patrickfoulger1@gmail.com>
2025-02-17 13:19:09 +0000

    add slider carouselle

--- src/Components/ArticleCarousel.jsx
+++ src/Components/ArticleCarousel.jsx
@@ -1,5 +1,40 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCreative } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-cards";
+
 function ArticleCarousel() {
-  return;
+  return (
+    <>
+      <Swiper
+        grabCursor={true}
+        effect={"creative"}
+        creativeEffect={{
+          prev: {
+            translate: ["-20%", 0, -1],
+          },
+          next: {
+            translate: ["100%", 0, 0],
+          },
+        }}
+        modules={[EffectCreative]}
+        className="mySwiper3"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
 
 export default ArticleCarousel;
--- src/Components/ArticleExplorer.jsx
+++ src/Components/ArticleExplorer.jsx
@@ -2,10 +2,9 @@ import ArticleCarousel from "./ArticleCarousel";
 
 function ArticleExplorer() {
   return (
-    <>
+    <section className="flex h-8/10 w-full" aria-label="Articles">
       <ArticleCarousel />
-      <p>To add article buttons...</p>;
-    </>
+    </section>
   );
 }
 
