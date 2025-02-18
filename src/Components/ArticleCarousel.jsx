074a326e39abc86556c56bb1d120a6603845dae6
patrickfoulger1 <patrickfoulger1@gmail.com>
2025-02-18 08:59:58 +0000

    install react router

--- src/Components/ArticleCarousel.jsx
+++ src/Components/ArticleCarousel.jsx
@@ -1,4 +1,4 @@
-import { Virtual, Pagination, Mousewheel } from "swiper/modules";
+import { Virtual, Pagination, Mousewheel, Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
@@ -6,9 +6,9 @@ import "swiper/css";
 import "swiper/css/virtual";
 import "swiper/css/pagination";
 import "swiper/css/free-mode";
+import "swiper/css/navigation";
 
 import ArticleCard from "./ArticleCard";
-import "swiper/css/pagination";
 
 function ArticleCarousel({ articles }) {
   const articleSlides = articles.map((article, index) => (
@@ -19,24 +19,41 @@ function ArticleCarousel({ articles }) {
 
   return (
     <Swiper
-      modules={[Virtual, Pagination, Mousewheel]}
+      style={{
+        "--swiper-pagination-color": "#FFBA08",
+        "--swiper-pagination-bullet-inactive-color": "#999999",
+        "--swiper-pagination-bullet-inactive-opacity": "1",
+        "--swiper-pagination-bullet-size": "8px",
+        "--swiper-pagination-bullet-horizontal-gap": "6px",
+      }}
+      modules={[Virtual, Pagination, Mousewheel, Navigation]}
       spaceBetween={0}
-      slidesPerView={3}
+      slidesPerView={2}
       direction={"vertical"}
       mousewheel={true}
+      navigation={false}
       freeMode={true}
       pagination={{
         dynamicBullets: true,
       }}
       breakpoints={{
-        640: { slidesPerView: 3, direction: "horizontal" },
+        376: {
+          slidesPerView: 3,
+        },
+
+        640: {
+          navigation: { enabled: true },
+          slidesPerView: 3,
+          direction: "horizontal",
+        },
         1028: {
           slidesPerView: 4,
           direction: "horizontal",
+          navigation: { enabled: true },
         },
         1920: {
           slidesPerView: 5,
-          direction: "horizontal",
+          navigation: { enabled: true },
         },
       }}
       virtual
