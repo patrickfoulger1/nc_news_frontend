a7aa5561690b9b7fddbefd9632c0aa25ae4b38e8
patrickfoulger1 <patrickfoulger1@gmail.com>
2025-02-21 13:12:17 +0000

    css

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -24,7 +24,14 @@ function ArticleCard({
 
   return (
     <figure
-      className={`w-full h-full`}
+    onClick={()=>{
+      navigate(`/${article_id}`);
+      setNavSettings((settings) => {
+      const newSettings = { ...settings };
+      newSettings.lastArticleIdClicked = article_id;
+      return newSettings;
+    });}}
+      className={`w-full h-full border-red-500 hover:border-2`}
      
     >
       <img
@@ -32,51 +39,38 @@ function ArticleCard({
         src={article_img_url}
         alt={title}
       />
-      <figcaption className="absolute relative bottom-1/2 lg:bottom-[calc(10%+7rem)] left-1/2  justify-center flex w-[calc(100%-2rem)] h-[calc(70%-4rem)] max-h-45 -translate-x-1/2 -translate-y-1/2 justify-between rounded-xl border border-white bg-white/90 py-4 px-3 shadow-lg shadow-black/5 ">
+      <figcaption  className="absolute relative bottom-1/2 lg:bottom-[calc(10%+7rem)] left-1/2  justify-center items-center flex w-[calc(100%-1rem)] h-[calc(70%-4rem)] max-h-45 -translate-x-1/2 -translate-y-1/2 justify-between rounded-xl border border-white bg-white/90 py-4 px-3 shadow-lg shadow-black/5 ">
         <div className="">
-          <h5 className="text-l md:text-l font-medium text-slate-800 text-center min-h-14 xl-text-lg">{title}</h5>
+          <h5 className="text-l md:text-l font-medium text-slate-800 text-center min-h-10 xl-text-lg">{title}</h5>
           <p className="mt-2 text-slate-600 text-sm  text-center">{getDate(created_at)}</p>
-          <p className="flex flex-rows items-center justify-center ">
+          <div className="flex flex-rows items-center justify-center ">
         
          
             <Star
-              className="text-2xl"
+              className="text-2xl mr-1"
               weight="fill"
               color="#FB2C36"
             ></Star>
-            <p className="text-xs mr-3 text-center ">{votes} likes </p>
+            <p className="text-l mr-3 text-center ">{votes} likes </p>
             <ChatCircleText
-              className="text-xl"
+              className="text-2xl mr-1"
               weight="fill"
               color="#FB2C36"
             ></ChatCircleText>
-            <p className="text-xs  text-center"> {comment_count} comments </p>
+            <p className="text-l  text-center"> {comment_count} comments </p>
             <Link
             to={{
               pathname: `/`,
               search: `?topic=${topic}&display=first${sortQuery}`,
             }}
           >
-            <p className="m-4 inline-block cursor-pointer bg-black rounded-sm p-1 text-white w-inherit hover:bg-white hover:text-black">
+            <button  className="z-10 m-4 inline-block cursor-pointer bg-black rounded-sm p-1 text-white w-inherit hover:bg-white hover:text-black">
               #{topic}
-            </p>
+            </button>
           </Link>
-          <p className="inline-block cursor-pointer bg-red-500 rounded-sm p-1 text-white text-center w-inherit hover:bg-white hover:text-black">
-            <a
-              onClick={() => {
-                navigate(`/${article_id}`);
-                setNavSettings((settings) => {
-                  const newSettings = { ...settings };
-                  newSettings.lastArticleIdClicked = article_id;
-                  return newSettings;
-                });
-              }}
-            >
-              Read Me
-            </a>
-          </p>
+         
           
-          </p>
+          </div>
             
          
           
